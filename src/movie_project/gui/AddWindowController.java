6d2225d066fa6cab8f3443501b2ce97d4af59576
langehk <Rupkri32@gmail.com>
2018-01-08 10:45:46 +0100

    newMovieWindow :100:

---
+++ src/movie_project/gui/AddWindowController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movie_project.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author morte
+ */
+public class AddWindowController implements Initializable
+{
+
+    @FXML
+    private ComboBox<?> comboCategory;
+    @FXML
+    private Button saveMovieBtn;
+    @FXML
+    private Button cancelMovieBtn;
+    @FXML
+    private TextField txtImdbRating;
+    @FXML
+    private TextField txtTitle;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        // TODO
+    }    
+
+    @FXML
+    private void clickAddAction(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void clickSaveAction(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void closeMovieAddBox(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void clickLoadCategoriesBtn(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void clickChooseFile(ActionEvent event)
+    {
+    }
+    
+}
