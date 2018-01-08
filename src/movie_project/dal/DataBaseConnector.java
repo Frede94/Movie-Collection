a13c2b5dae251afa6706a596bf219cc6942d7bca
Mikkel K <mikk6578@easv365.dk>
2018-01-08 10:21:21 +0100

    DataBaseConnector

--- src/movie_project/dal/DataBaseConnector.java
+++ src/movie_project/dal/DataBaseConnector.java
@@ -5,11 +5,33 @@
  */
 package movie_project.dal;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
+
 /**
  *
  * @author Frederik Bærbar
  */
 public class DataBaseConnector
 {
-    
+
+    /**
+     * Creates a connection to the database user.
+     */
+    private SQLServerDataSource ds = new SQLServerDataSource();
+
+    public DataBaseConnector()
+    {
+        ds.setDatabaseName("MovieMMEF");
+        ds.setUser("CS2017A_25_java");
+        ds.setPassword("javajava");
+        ds.setServerName("EASV-DB2");
+        ds.setPortNumber(1433);
+    }
+
+    public Connection getConnection() throws SQLServerException
+    {
+        return ds.getConnection();
+    }
 }
