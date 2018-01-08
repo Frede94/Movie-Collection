45f0b0ea782e7faaab653705162efd123451b321
langehk <Rupkri32@gmail.com>
2018-01-08 12:46:39 +0100

    filechooser

--- src/movie_project/gui/AddWindowController.java
+++ src/movie_project/gui/AddWindowController.java
@@ -5,14 +5,20 @@
  */
 package movie_project.gui;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import movie_project.be.Movies;
 
 /**
@@ -75,6 +81,19 @@ public class AddWindowController implements Initializable
     @FXML
     private void clickChooseFile(ActionEvent event)
     {
+        
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Open File");
+        chooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("*.mp4", "*.mpeg4"));
+        File file = chooser.showOpenDialog(new Stage());
+        try
+        {
+            String fullPath = file.getCanonicalPath();
+            txtPath.setText(fullPath);
+        } catch (IOException ex)
+        {
+            Logger.getLogger(AddWindowController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
