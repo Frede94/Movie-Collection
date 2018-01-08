b779b419e4eb2b32902d4cddbe16f063d53373b7
Frede94 <Frede94@users.noreply.github.com>
2018-01-08 10:02:42 +0100

    Mapper

--- src/movie_project/MovieCollection.java
+++ src/movie_project/MovieCollection.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package movie.collection;
+package movie_project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
